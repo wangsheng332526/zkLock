ec4deefde2c6ee0929fcb59f5bc68a5456910745
wangsheng332526 <46277020+wangsheng332526@users.noreply.github.com>
2019-01-02 14:56:01 +0800

    Add files via upload

---
+++ UserService.java
@@ -0,0 +1,22 @@
+
+ /**************************************************************************
+ * Copyright (c) 2016-2020 ZheJiang International E-Commerce Services Co.,Ltd. 
+ * All rights reserved.
+ * 
+ * 名称：kafka
+ * 版权说明：本软件属于浙江国贸云商企业服务有限公司所有，在未获得浙江国贸云商企业服务有限公司正式授权
+ *           情况下，任何企业和个人，不能获取、阅读、安装、传播本软件涉及的任何受知
+ *           识产权保护的内容。                            
+ ***************************************************************************/
+package com.test.lock;
+
+
+ /**
+  * TODO 请在此处添加注释
+  * @author <a href="mailto:"wangsheng"@zjiec.com”>"wangsheng"</a>
+  * @version 2019年1月1日  下午2:01:38  
+  * @since 2.0
+  */
+public interface UserService {
+	public void sayName(String userName);
+}
